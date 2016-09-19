3b62da22afc550a27d95a42d2e7df2195119f3ed
Mitchell Thompson <leonsbuddydave@gmail.com>
2016-09-18 18:10:19 -0700

    Updated defs

---
+++ definitions/google.js
@@ -0,0 +1,12 @@
+// @name Google
+// @include google.com
+
+(function() {
+	var exports = {};
+
+	exports["Do A Google"] = {
+		action: function() { console.log("GOOGLE") }
+	}
+
+	return exports;
+})();
---
+++ definitions/test-map.js
@@ -0,0 +1,14 @@
+// @name Gunt Overflow
+// @include stack
+
+(function() {
+	var exports = {};
+
+	exports["Navigate: Tour"] = {
+		action: function() {
+			console.log("MR TOUR");
+		}
+	}
+
+	return exports;
+})();
